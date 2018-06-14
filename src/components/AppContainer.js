b36e56130637207001da908b4c0e49e77e6cb3cf
Mat Greten <mgreten@gmail.com>
2018-06-14 09:07:54 -0600

    added calculateInterestYears

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -20,10 +20,21 @@ class AppContainer extends Component {
         this.setState({comfortable_expenses: comfortValue});
     };
 
+    static calculateInterestYears(current, future) {
+        let temp = current;
+        let years = 0;
+
+        for (years; temp <= future; years++) {
+            temp = (temp * .08) + temp;
+            console.log(temp);
+        }
+        return(years)
+    }
+
     render() {
         let nf = new Intl.NumberFormat();
-        let leanFi = this.state.core_expenses * 25;
         let halfFi = this.state.comfortable_expenses * 12.5;
+        let leanFi = this.state.core_expenses * 25;
         let flexFi= this.state.comfortable_expenses * 20;
         let fi = this.state.comfortable_expenses * 25;
         let fatFi = this.state.comfortable_expenses * 30;
@@ -40,24 +51,49 @@ class AppContainer extends Component {
                 <h3>FI Calculations</h3>
                 <ListGroup>
                     <ListGroupItem>
-                        <h4>Lean FI: ${nf.format(leanFi)}</h4>
-                        <i>25x core expenses</i>
-                    </ListGroupItem>
-                    <ListGroupItem>
                         <h4>Half FI: ${nf.format(halfFi)}</h4>
                         <i>12.5x comfortable expenses</i>
+                        <div>
+                            <p>Interest alone will carry you to Lean Fi in {AppContainer.calculateInterestYears(halfFi,leanFi)} years. <br/>
+                                <i>*Assumes 8% ROI.</i>
+                            </p>
+                        </div>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        <h4>Lean FI: ${nf.format(leanFi)}</h4>
+                        <i>25x core expenses</i>
+                        <div>
+                            <p>Interest alone will carry you to Flex Fi in {AppContainer.calculateInterestYears(leanFi, flexFi)} years. <br/>
+                                <i>*Assumes 8% ROI.</i>
+                            </p>
+                        </div>
                     </ListGroupItem>
                     <ListGroupItem>
                         <h4>Flex FI: ${nf.format(flexFi)}</h4>
                         <i>20x comfortable expenses</i>
+                        <div>
+                            <p>Interest alone will carry you to Fi in {AppContainer.calculateInterestYears(flexFi, fi)} years. <br/>
+                                <i>*Assumes 8% ROI.</i>
+                            </p>
+                        </div>
                     </ListGroupItem>
                     <ListGroupItem>
                         <h4>FI: ${nf.format(fi)}</h4>
                         <i>25x comfortable expenses</i>
+                        <div>
+                            <p>Interest alone will carry you to Fat Fi in {AppContainer.calculateInterestYears(fi, fatFi)} years. <br/>
+                                <i>*Assumes 8% ROI.</i>
+                            </p>
+                        </div>
                     </ListGroupItem>
                     <ListGroupItem>
                         <h4>FAT FI: ${nf.format(fatFi)}</h4>
                         <i>30x comfortable expenses</i>
+                        <div>
+                            <p>Interest alone will carry you to Lean Fi in {AppContainer.calculateInterestYears(halfFi,leanFi)} years. <br/>
+                                <i>*Assumes 8% ROI.</i>
+                            </p>
+                        </div>
                     </ListGroupItem>
                 </ListGroup>
             </div>
